9aa3ee082877356303f43bd31ea720e14361aed7
shahadathhs <shahadathhossensajib732@gmail.com>
2024-10-15 21:57:40 +0600

    first commit

---
+++ src/Learn.java
@@ -0,0 +1,10 @@
+public class Learn {
+    public static void main(String[] args) {
+        System.out.println("First Java Console");
+
+        System.out.print("Second Java Console");
+        System.out.print("Third Java Console");
+
+        System.out.println("Third Java Console");
+    }
+}
