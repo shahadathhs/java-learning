81bb6de870462bb8a06c428ed04c6d6f676e35e9
shahadathhs <shahadathhossensajib732@gmail.com>
2024-10-17 07:19:20 +0600

    feat: data type basic completed

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -26,5 +26,21 @@ public class DataTypes {
         double d1 = 12E4d;
         System.out.println(f1);
         System.out.println(d1);
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+
+        char myGrade = 'B';
+        System.out.println(myGrade);
+
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+
+        String greeting = "Hello World";
+        System.out.println(greeting);
     }
 }
